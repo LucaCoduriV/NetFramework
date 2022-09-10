80c12978113ef1042814ccfa3edc126691133567
Luca <coduri.vitor@gmail.com>
2022-09-10 14:30:01 +0200

    Base
    Base

---
+++ NetFramework/include/NetFramework.hpp
@@ -0,0 +1,26 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier  : NetFramework.hpp
+Auteur(s)       : lucac
+Date creation   : 10.09.2022
+Description     : <à compléter>
+Remarque(s)     : <à compléter>
+Compilateur     : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------
+
+*/
+
+#ifndef NETFRAMEWORK_NETFRAMEWORK_HPP
+#define NETFRAMEWORK_NETFRAMEWORK_HPP
+
+#include <string>
+
+class NetFramework {
+public:
+   static std::string test() {
+      return "coucou";
+   }
+};
+
+
+#endif //NETFRAMEWORK_NETFRAMEWORK_HPP
---
+++ Playground/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <NetFramework.hpp>
+
+int main() {
+   std::cout << NetFramework::test();
+   std::cout << "Hello, World!" << std::endl;
+   return 0;
+}
